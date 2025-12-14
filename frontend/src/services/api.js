11142858e80dcb85f9f23dd58be1015864b1333c
Samarpreet Singh <psamar142@gmail.com>
2025-12-14 22:45:39 +0530

    api error fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: String(import.meta.env.VITE_BACKEND_URI || "http://localhost:5000/api"),
 }, { withCredentials: true });
 
 export const apiConnector = async (method, url, data = {}, headers, config = {}) => {
